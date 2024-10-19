a2ab9d333eaca200e1a871d6cee3089e2c508b52
Varsha Sundar <85093623+varsha-sundar@users.noreply.github.com>
2024-10-19 20:32:59 +0530

    Latest
    Navigation menu
    About page
    Summary modal
    Improved control panel
    Play/pause functionality added
    Mobile banner - View on desktop

--- travel-map/travel-map-script.js
+++ travel-map/travel-map-script.js
@@ -1,7 +1,8 @@
+
 const TravelMap = {
-    // Constants
+    // API key for OpenRouteService
     API_KEY: '5b3ce3597851110001cf6248f2cd33880b1940c2853bd933973f70de',
-    EARTH_RADIUS: 6371, // km
+    EARTH_RADIUS: 6371, // km used for distance calculation
 
     // Properties
     map: null,
@@ -9,8 +10,23 @@ const TravelMap = {
     markerLayer: null,
     currentSegment: 0,
     visibleLocationIndices: [],
+
+    // Flag to indicate animation status
     animationInProgress: false,
     animationStopped: false,
+    animationPause: false,
+
+    // Nav menu elements
+    navMenuBar: null,
+    navMenuIcon: null,
+    navSideDrawer: null,
+    navDrawerHeader: null,
+    navDrawerIcon: null,
+    navOverlay: null,
+    aboutMenuItem: null,
+    aboutPage: null,
+    aboutClose: null,
+    mapOverlay: null,
 
     // Trip colors
     tripColors: {
@@ -32,6 +48,7 @@ const TravelMap = {
         newEnglandEscape: '#ADD8E6'
     },
 
+    // Load travel data from JSON file
     async loadTravelData() {
         try {
             console.log('Attempting to fetch travel data...');
@@ -56,14 +73,17 @@ const TravelMap = {
         }
     },
 
+    // Initialize the Leaflet map
     initMap() {
         this.map = L.map('map', {
             center: [20, 0],
             zoom: 2,
             zoomSnap: 0.1,
-            zoomDelta: 0.5,
+            zoomDelta: 1.5,
+            zoomAnimation: true,
+            zoomAnimationThreshold: 4,
             wheelDebounceTime: 40,
-            wheelPxPerZoomLevel: 80
+            wheelPxPerZoomLevel: 30
         });
     
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -80,8 +100,16 @@ const TravelMap = {
         }
     
         this.setupSmoothZoom();
-    },
 
+        const modal = document.getElementById('summary-modal');
+        const closeBtn = modal.querySelector('.close');
+        closeBtn.onclick = () => this.closeSummaryModal();
+        window.onclick = (event) => {
+          if (event.target === modal) {
+            this.closeSummaryModal();
+          }
+        };
+      },
 
     setupSmoothZoom() {
         this.map.on('zoomstart', () => this.map.smoothZoom = true);
@@ -99,6 +127,124 @@ const TravelMap = {
         };
     },
 
+    // Toggle Navigation Menu with hamburger icon
+    toggleNavMenu() {
+        this.navSideDrawer.classList.toggle('open');
+        this.navOverlay.style.display = this.navSideDrawer.classList.contains('open') ? 'block' : 'none';
+        this.navMenuIcon.innerHTML = this.navSideDrawer.classList.contains('open') ? '✕' : '☰';
+        this.navDrawerIcon.innerHTML = this.navSideDrawer.classList.contains('open') ? '✕' : '☰';
+    },
+    
+    closeNavMenu() {
+        this.navSideDrawer.classList.remove('open');
+        this.navOverlay.style.display = 'none';
+        this.navMenuIcon.innerHTML = '☰';
+        this.navDrawerIcon.innerHTML = '☰';        
+    },
+
+    // Initialize navigation menu and related components
+    initNavMenu() {
+        this.navMenuBar = document.getElementById('nav-menu-bar');
+        this.navMenuIcon = document.getElementById('nav-menu-icon');
+        this.navSideDrawer = document.getElementById('nav-side-drawer');
+        this.navDrawerHeader = document.getElementById('nav-drawer-header');
+        this.navDrawerIcon = document.getElementById('nav-drawer-icon');
+        this.navOverlay = document.getElementById('nav-overlay');
+
+        this.navMenuBar.addEventListener('click', () => this.toggleNavMenu());
+        this.navDrawerHeader.addEventListener('click', () => this.toggleNavMenu());
+        this.navOverlay.addEventListener('click', () => this.closeNavMenu());
+        this.initAboutPage();
+
+        const summaryMenuItem = document.getElementById('summary-menu-item');
+        summaryMenuItem.addEventListener('click', () => {
+          this.showSummaryModal();
+          this.closeNavMenu();
+        });
+      },
+
+    initAboutPage() {
+        this.aboutMenuItem = document.getElementById('about-menu-item');
+        this.aboutPage = document.getElementById('about-page');
+        this.aboutClose = document.getElementById('about-close');
+        this.mapOverlay = document.getElementById('map-overlay');
+
+        console.log('Init About Page');
+        console.log('About menu item:', this.aboutMenuItem);
+        console.log('About page:', this.aboutPage);
+        console.log('About close:', this.aboutClose);
+        console.log('Map overlay:', this.mapOverlay);    
+      
+        if (this.aboutMenuItem && this.aboutPage && this.aboutClose && this.mapOverlay) {
+            this.aboutMenuItem.addEventListener('click', (e) => {
+                e.stopPropagation();
+                console.log('About menu item clicked');
+                this.toggleAboutPage();   
+            });     
+            this.aboutClose.addEventListener('click', () => this.closeAboutPage());
+            this.mapOverlay.addEventListener('click', () => this.closeAboutPage());
+            console.log('Event listeners added');
+        } else {
+            console.error('One or more About page elements are missing');
+        }
+    },
+      
+    openAboutPage() {
+        console.log('Opening About page');
+        console.log('About page element before:', this.aboutPage);
+        console.log('About page classList before:', this.aboutPage.classList);    
+        
+        if (this.animationInProgress) {
+            this.stopAnimation();
+        }
+        
+        const timeline = document.getElementById('timeline');
+        if (timeline) {
+            timeline.style.display = 'none';
+        }
+        
+        if (this.aboutPage) {
+            this.aboutPage.classList.add('open');
+            console.log('About page classList after:', this.aboutPage.classList);
+        } else {
+            console.error('About page element not found');
+        }
+        
+        if (this.mapOverlay) {
+            this.mapOverlay.style.display = 'block';
+            console.log('Map overlay display:', this.mapOverlay.style.display);
+        } else {
+            console.error('Map overlay element not found');
+        }
+    
+        console.log('About page classList after:', this.aboutPage.classList);
+    
+    },
+                
+    closeAboutPage() {
+        console.log('Closing About page');
+        console.log('About page classList before:', this.aboutPage.classList);
+    
+        this.aboutPage.classList.remove('open');
+        this.mapOverlay.style.display = 'none';
+
+        console.log('About page classList after:', this.aboutPage.classList);
+        console.log('Map overlay display:', this.mapOverlay.style.display);    
+        
+        const timeline = document.getElementById('timeline');
+        if (timeline) {
+          timeline.style.display = 'block';
+        }
+    },
+
+    toggleAboutPage() {
+        if (this.aboutPage.classList.contains('open')) {
+            this.closeAboutPage();
+        } else {
+            this.openAboutPage();
+        }
+    },    
+    
     async fetchRoute(start, end, type) {
         const profile = type === 'train' ? 'driving-car' : 'driving-car';
         const url = `https://api.openrouteservice.org/v2/directions/${profile}?api_key=${this.API_KEY}&start=${start.lon},${start.lat}&end=${end.lon},${end.lat}`;
@@ -135,8 +281,7 @@ const TravelMap = {
     createPopupContent(location) {
         return `
             <div class="popup-content">
-                <div class="popup-location">${location.name}, ${location.state ? location.state + ', ' : ''}${location.country}</div>
-                <div class="popup-date">${this.formatDate(location.date)}</div>
+                <div class="popup-location">${location.name}</div>
             </div>
         `;
     },
@@ -288,10 +433,25 @@ const TravelMap = {
     },
 
     async startAnimation() {
+        if (this.animationInProgress && this.animationPaused) {
+            // Resume animation
+            this.animationPaused = false;
+            document.getElementById('playPauseBtn').textContent = 'Pause';
+            return;
+        }
+
         this.resetAnimation();
         this.animationInProgress = true;
         this.animationStopped = false;
+        this.animationPaused = false;
+        document.getElementById('playPauseBtn').textContent = 'Pause';
+
         for (let i = 0; i < this.travelPath.length - 1; i++) {
+            if (this.animationStopped) break;
+            while (this.animationPaused) {
+                await new Promise(resolve => setTimeout(resolve, 100));
+                if (this.animationStopped) break;
+            }
             if (this.animationStopped) break;
             await this.animateSegment();
             if (this.animationStopped) break;
@@ -299,10 +459,27 @@ const TravelMap = {
         }
         this.showFullView();
         this.animationInProgress = false;
+        this.showSummaryModal();
+        document.getElementById('playPauseBtn').textContent = 'Play';
     },
 
     stopAnimation() {
         this.animationStopped = true;
+        this.animationPaused = false;
+        this.animationInProgress = false;
+        document.getElementById('playPauseBtn').textContent = 'Play';
+    },
+
+    togglePlayPause() {
+        if (!this.animationInProgress) {
+            this.startAnimation();
+        } else if (this.animationPaused) {
+            this.animationPaused = false;
+            document.getElementById('playPauseBtn').textContent = 'Pause';
+        } else {
+            this.animationPaused = true;
+            document.getElementById('playPauseBtn').textContent = 'Play';
+        }
     },
 
 
@@ -311,35 +488,44 @@ const TravelMap = {
         this.map.fitBounds(bounds, { padding: [50, 50] });
     },
 
-    showAllRoutes() {
+    async showAllRoutes() {
         this.resetAnimation();
-        this.travelPath.forEach((location, index) => {
-            if (index < this.travelPath.length - 1) {
-                const start = location;
-                const end = this.travelPath[index + 1];
-                const color = this.getColor(end.type);
-                const dashArray = end.type === "fly" ? '10, 10' : null;
-
-                if (end.type === 'fly') {
-                    const route = this.createArc([start.lat, start.lon], [end.lat, end.lon], 100);
-                    L.polyline(route, { color, dashArray }).addTo(this.pathLayer);
-                } else {
-                    L.polyline([[start.lat, start.lon], [end.lat, end.lon]], { color, dashArray }).addTo(this.pathLayer);
-                }
-            }
-            this.addMarker(location);
-        });
+        for (let i = 0; i < this.travelPath.length - 1; i++) {
+          const start = this.travelPath[i];
+          const end = this.travelPath[i + 1];
+          const color = this.getColor(end.type);
+          const dashArray = end.type === "fly" ? '10, 10' : null;
+      
+          let route;
+          if (end.type === 'drive' || end.type === 'train') {
+            route = await this.fetchRoute(start, end, end.type);
+          } else if (end.type === 'fly') {
+            route = this.createArc([start.lat, start.lon], [end.lat, end.lon], 100);
+          } else {
+            route = [[start.lat, start.lon], [end.lat, end.lon]];
+          }
+      
+          L.polyline(route, { color, dashArray }).addTo(this.pathLayer);
+          this.addMarker(start);
+        }
+        this.addMarker(this.travelPath[this.travelPath.length - 1]);
         this.showFullView();
-    },
 
-    resetAnimation() {
-        this.currentSegment = 0;
-        this.pathLayer.clearLayers();
-        this.markerLayer.clearLayers();
-        this.addMarker(this.travelPath[0], "🏠");
-        this.map.setView([20, 0], 2);
-        this.clearTimelineHighlights();
-    },
+          // Trigger the summary modal after showing all routes
+        setTimeout(() => {
+            this.showSummaryModal();
+        }, 1000);  // Delay of 1 second to allow the map to settle
+        },
+
+        resetAnimation() {
+            this.stopAnimation();
+            this.currentSegment = 0;
+            this.pathLayer.clearLayers();
+            this.markerLayer.clearLayers();
+            this.addMarker(this.travelPath[0], "🏠");
+            this.map.setView([20, 0], 2);
+            this.clearTimelineHighlights();
+        },
 
     createTimeline() {
         const timeline = document.getElementById('timeline');
@@ -529,7 +715,7 @@ const TravelMap = {
         }
       },
       
-      updateExpandIcons(openLocationName) {
+    updateExpandIcons(openLocationName) {
         const items = document.querySelectorAll('.timeline-item');
         items.forEach(item => {
           const icon = item.querySelector('.expand-icon');
@@ -565,12 +751,116 @@ const TravelMap = {
         document.getElementById('fullsize-image').style.display = 'none';
       },
 
+      //This is for the summary pop-up page
+      showSummaryModal() {
+        this.closeNavMenu();
+        this.closeAboutPage();      
+        const modal = document.getElementById('summary-modal');
+        modal.style.display = 'block';
+        this.populateSummaryContent();
+      },
+      
+      closeSummaryModal() {
+        const modal = document.getElementById('summary-modal');
+        modal.style.display = 'none';
+      },
+
+      populateSummaryContent() {
+        const summaryContent = document.getElementById('summary-content');
+        const distanceByTransport = this.calculateDistanceByTransport();
+        const totalDistance = Object.values(distanceByTransport).reduce((a, b) => a + b, 0);
+      
+        let distanceBreakdown = '';
+        for (let type in distanceByTransport) {
+          if (distanceByTransport[type] > 0) {
+            distanceBreakdown += `<li>${type.charAt(0).toUpperCase() + type.slice(1)}: ${distanceByTransport[type]} km</li>`;
+          }
+        }
+      
+        summaryContent.innerHTML = `
+          <p><strong>Distance breakdown</strong></p>
+          <ul>
+            ${distanceBreakdown}
+          </ul>
+          <p><strong>Total = </strong> 60944 km</p>
+          <p><strong>Number of cities visited:</strong> 46</p>
+          <p><strong>Coldest day:</strong><br> 5°C in Tre Cime National Park, Dolomites on Sep 20</p>
+          <p><strong>Hottest temperature:</strong><br> 42°C in Zion National Park, Utah on Jul 31</p>
+          <p><strong>Highest point:</strong><br> 2528m at Rifugio Pian di Cengia, Dolomites on Sep 21</p>
+          <p><strong>Lowest point:</strong><br> -30m below sea level at Liberty Wreck, Tulamben, Bali on Apr 14</p>
+        `;
+      },
+      
+      //These are to calculate some fun stats to display
+      calculateDistanceByTransport() {
+        const distanceByType = {
+          fly: 0,
+          drive: 0,
+          train: 0,
+        };
+      
+        for (let i = 0; i < this.travelPath.length - 1; i++) {
+          const start = this.travelPath[i];
+          const end = this.travelPath[i + 1];
+          const distance = this.calculateDistance(start, end);
+      
+          // Use the type of the destination (end) as the travel type
+          const travelType = end.type || 'other';
+      
+          if (distanceByType.hasOwnProperty(travelType)) {
+            distanceByType[travelType] += distance;
+          } else {
+            distanceByType.other += distance;
+          }
+        }
+      
+        // Round all distances
+        for (let type in distanceByType) {
+          distanceByType[type] = Math.round(distanceByType[type]);
+        }
+      
+        return distanceByType;
+      },
+
 };
 
 // Initialize map when the script loads
 document.addEventListener('DOMContentLoaded', () => {
     TravelMap.loadTravelData();
+    TravelMap.initNavMenu();
+
+    const playPauseBtn = document.getElementById('playPauseBtn');
+    const resetBtn = document.getElementById('resetBtn');
+
+    playPauseBtn.addEventListener('click', () => TravelMap.togglePlayPause());
+    resetBtn.addEventListener('click', () => TravelMap.resetAnimation());
+
 });
+
+document.addEventListener('click', (e) => {
+    console.log('Clicked element:', e.target);
+});
+
+// Mobile banner
+document.addEventListener('DOMContentLoaded', function() {
+    const banner = document.getElementById('mobile-banner');
+    const closeBanner = document.getElementById('close-banner');
+  
+    function checkScreenSize() {
+      if (window.innerWidth <= 768) {
+        banner.style.display = 'block';
+      } else {
+        banner.style.display = 'none';
+      }
+    }
+  
+    closeBanner.addEventListener('click', function() {
+      banner.style.display = 'none';
+    });
+  
+    window.addEventListener('resize', checkScreenSize);
+    checkScreenSize(); // Check on initial load
+  });  
 
 // Expose necessary functions globally
 window.startAnimation = () => {
@@ -580,4 +870,8 @@ window.startAnimation = () => {
 };
 window.stopAnimation = () => TravelMap.stopAnimation();
 window.resetAnimation = () => TravelMap.resetAnimation();
-window.showAllRoutes = () => TravelMap.showAllRoutes();
+window.showAllRoutes = () => {
+    TravelMap.showAllRoutes().catch(error => console.error('Error showing all routes:', error));
+  };
+
+  
